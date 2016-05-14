3c894e2a511a2b591ba92ca68b2266be91be8ecb
suguineng <suguineng@126.com>
2016-05-14 15:34:57 +0800

    editor: 增加认证管理

--- proc_editor.py
+++ proc_editor.py
@@ -16,13 +16,26 @@ class data:
 	def _page(self):
 		self._http.regfun("/editor/file_get", self._file_get);
 		self._http.regfun("/editor/file_save", self._file_save);
+		self._http.regfun("/auth/login", self._auth_login);
 		return
 	
+	def _auth_login(self, cgi):
+	    a_dict = {'code': 0};
+	    
+	    print cgi['session']
+	    if cgi['session'].get('username'):
+	        print("login:" + cgi['session'].get('username')); 
+	    else:
+	        cgi['session']['username'] = cgi['username'];
+	        
+	    return a_dict
+	    
 	# 获取文件内容
 	def _file_get(self, cgi):
 		a_dict = {'name': 'test.txt',  'last':'2016-05-02 00:00:00',  'data': 'hello!world'}
 		a_dict['data'] = str(cgi)
 		
+		print a_dict
 		return demjson.encode(a_dict) 
 	
 	# 文件保存
