ab843220e2bbcf281c54bf677acb7f5d5c2c93a7
suguineng <suguineng@126.com>
2016-05-16 14:03:24 +0800

    editor: 增加文件系统保存功能

--- proc_editor.py
+++ proc_editor.py
@@ -49,8 +49,7 @@ class data:
             a_item['type'] = "file";
             a_item['text'] = a_file.replace("\n","");
             a_dict['data'].append(a_item);
-            
-        print a_dict
+        
         return a_dict
          
     # 获取文件内容
@@ -60,14 +59,21 @@ class data:
         
         if not self._user_loginin(cgi):
             a_dict['code'] = -2;
+            return a_dict;
         
         a_dict['name'] = cgi['name'];
+        
+        # 获取文件内容
         if os.path.exists(a_path):
             with open(a_path, 'r') as f:
                 a_dict['data'] = f.read(-1)
+            a_stat = os.stat(a_path);
+            a_dict['last'] = str(a_stat.st_mtime);
+            
         else:
             # 默认值
             a_dict['data'] = "# " + a_dict['name'] + " #" + "\n\n"
+        
         return a_dict
     
     # 文件保存
@@ -76,17 +82,36 @@ class data:
         a_path = self._top + "/" + cgi['name'];
         a_data = cgi['data']
         
+        # 未登录,需要重新登录
+        if not self._user_loginin(cgi):
+            a_dict['code'] = -2;
+            return a_dict;
+                    
         # 创建目录
         a_dir = os.path.dirname(a_path)
         if not os.path.exists(a_dir):
             os.makedirs(a_dir);
         
+        # 如果路径存在,则判断文件是否已经被修改过
+        if os.path.exists(a_path):
+            a_stat = os.stat(a_path);
+            print("exitsts", a_path, cgi['last'], a_stat.st_mtime);
+            if str(a_stat.st_mtime) != cgi['last']:
+            	a_dict['code'] = -1	# 返回失败
+            	a_dict['last'] = str(a_stat.st_mtime);
+            	a_dict['error'] = "file has change"
+            	return a_dict
+            
         # 保存
-        print a_path
         with open(a_path, 'w+b') as f:
             f.write(a_data)
             f.close();
             
+        # 获新获取最后一次时间
+        if os.path.exists(a_path):
+            a_stat = os.stat(a_path);
+            a_dict['last'] = str(a_stat.st_mtime);
+        
         return a_dict
     
     # 文件删除
